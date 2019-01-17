420e9d5cecf574a35634c3ed6adf72664ca0137a
GuaHsu <guaswork@gmail.com>
2019-01-17 22:21:58 +0800

    67 - use css module

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -410,7 +410,7 @@ module.exports = function(webpackEnv) {
                 importLoaders: 1,
                 sourceMap: isEnvProduction && shouldUseSourceMap,
                 modules: true,
-                getLocalIdent: getCSSModuleLocalIdent,
+                localIdentName: '[name]__[local]__[hash:base64:5]'
               }),
             },
             // Opt-in support for SASS (using .scss or .sass extensions).
@@ -441,7 +441,7 @@ module.exports = function(webpackEnv) {
                   importLoaders: 2,
                   sourceMap: isEnvProduction && shouldUseSourceMap,
                   modules: true,
-                  getLocalIdent: getCSSModuleLocalIdent,
+                  localIdentName: '[name]__[local]__[hash:base64:5]'
                 },
                 'sass-loader'
               ),
