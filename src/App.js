dbcf2afa8d5ff2475950e9986d6be58fa38d5e8e
GuaHsu <guaswork@gmail.com>
2019-01-17 21:40:12 +0800

    65 - add dynamic css/pesudo with Radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
+import Radium, { StyleRoot } from 'radium'
 
 class App extends Component {
   state = {
@@ -45,10 +46,15 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
-      padding: '8px'
+      padding: '8px',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null
@@ -66,20 +72,36 @@ class App extends Component {
           })}
         </div>
       )
+      style.backgroundColor = 'red'
+      style[':hover'] = {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
+    let classes = []
+    if (this.state.persons.length <= 2) {
+      classes.push('red')
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
+    }
+ 
     return (
-      <div className="App">
-        <h1>Hi, it's React App Here</h1>
-        <button
-          style={style}
-          onClick={this.togglePersonHandler}>
-          Switch Name
-        </button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, it's React App Here</h1>
+          <p className={classes.join(' ')}>This is really working !</p>
+          <button
+            style={style}
+            onClick={this.togglePersonHandler}>
+            Switch Name
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
